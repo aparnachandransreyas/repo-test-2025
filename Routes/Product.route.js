27da0a2972a8a80b6cb178e7bb8843e94fc8c5b2
aparnachandransreyas <aparnachandran886@gmail.com>
2025-02-03 00:04:45 +0530

    Update Product.route.js

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const express = require('express');
+const router = express.Router();
 
-const ProductSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true
-  }
-});
+const ProductController = require('../Controllers/Product.Controller');
 
-const Product = mongoose.model('product', ProductSchema);
-module.exports = Product;
+//Get a list of all products
+router.get('/', ProductController.getAllProducts);
+
+//Create a new product
+router.post('/', ProductController.createNewProduct);
+
+//Get a product by id
+router.get('/:id', ProductController.findProductById);
+
+//Update a product by id
+router.patch('/:id', ProductController.updateAProduct);
+
+//Delete a product by id
+router.delete('/:id', ProductController.deleteAProduct);
+
+module.exports = router;
